8543a72b8e5ddef4826083eeb8f0436731b14616
Sosin Vitalii <vitaliy.sosin@domrf.ru>
2021-06-26 19:34:14 +0300

    Container table cell

---
+++ Improving the application/UserStories/MainScreen/Cells/MainScreenBannerCell.swift
@@ -0,0 +1,14 @@
+//
+//  MainScreenBannerCell.swift
+//  Improving the application
+//
+//  Created by Vitalii Sosin on 26.06.2021.
+//
+
+import UIKit
+
+struct MainScreenBannerCellInsets: ContainerInsets {
+    static var insets: UIEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+}
+
+typealias MainScreenBannerCell = ContainerTableCell<UIButton, MainScreenBannerCellInsets>
